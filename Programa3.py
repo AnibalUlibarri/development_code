7f2fba5a545b691168deb4c11be6363f6453aaf9
Anibal Ulibarri <anibal.itsz.sistemas91@gmail.com>
2020-08-04 19:29:24 -0500

    cambios

--- Programa3.py
+++ Programa3.py
@@ -7,12 +7,12 @@
     Fecha: Jueves  8 de Marzo del 2018
 '''
 def calcula(txt):
-        x=0
-	print ("Haz introducido: ", txt)
+	x=0
+	print("Haz introducido: ", txt)
 	for i in txt:
-		print  str(i),(" Posición "), str(x)
+		print(str(i)," Posición ", str(x))
 		x+=1
-	print ("Total de letras: "), str(x)
+	print("Total de letras: ", str(x))
 if  __name__ == "__main__":
-	eltxt = raw_input("Introduce una palabra: ")
+	eltxt = input("Introduce una palabra: ")
 	calcula(eltxt)                               #'''add total: 10'''
